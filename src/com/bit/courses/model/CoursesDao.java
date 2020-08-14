1b8c2566f7e3fc52c0c557bf3ce13aa91f745dbc
js <dolph037@gmail.com>
2020-08-14 12:35:55 +0900

    1

---
+++ src/com/bit/courses/model/CoursesDao.java
@@ -0,0 +1,53 @@
+package com.bit.courses.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoursesDao {
+	String driver =	"oracle.jdbc.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String user = "scott";
+	String password = "tiger";
+	Connection conn;
+	Statement stmt;
+	ResultSet rs;
+	
+	public List<CoursesDto> getList() {
+		String sql = "select cno, ctitle, cbegin, cend from crs ORDER BY cno DESC";
+		List<CoursesDto> list = new ArrayList<CoursesDto>();
+		try {
+			System.out.println(sql);
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, password);
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				CoursesDto bean = new CoursesDto();
+				bean.setCno(rs.getInt("cno")); 
+				bean.setCtitle(rs.getString("ctitle"));
+				bean.setCbegin(rs.getDate("cbegin"));
+				bean.setCend(rs.getDate("cend"));
+				list.add(bean);				
+			}			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn!=null)conn.close();
+				if(stmt!=null)stmt.close();
+				if(rs!=null)rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+		
+	}
+}
