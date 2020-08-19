67f5932d6428729acba55e5bbde5a576e40cb350
JunCamp <junxdev@gmail.com>
2020-08-19 18:15:28 +0900

    수강신청 기능 완료, 반 개설 확정 미완, 파일 추가

---
+++ src/com/bit/std/model/MbrDto.java
@@ -0,0 +1,60 @@
+package com.bit.std.model;
+
+public class MbrDto {
+	private String id;
+	private String pw;
+	private String name;
+	private String tel;
+	private String email;
+	private String answer;
+	private String lvl;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getLvl() {
+		return lvl;
+	}
+	public void setLvl(String lvl) {
+		this.lvl = lvl;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", pw=" + pw + ", name=" + name
+				+ ", tel=" + tel + ", email=" + email + ", answer=" + answer
+				+ ", lvl=" + lvl + "]";
+	}
+}
